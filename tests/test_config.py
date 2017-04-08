d66fce10d81764aa60b39dbaac54472991274928
Flavian Hautbois <flavianh@theodo.fr>
2017-04-08 17:10:32 +0200

    Fix config test

--- tests/test_config.py
+++ tests/test_config.py
@@ -2,5 +2,5 @@ from traxit_manage.config import configure_database
 
 
 def test_configure_database():
-    db = configure_database()
+    db = configure_database('dbname')
     assert db is not None
